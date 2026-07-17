1cc75f071809fd6a9f193e43ca2a91b1389c9496
Prashant Verma <36754148+prashantpv08@users.noreply.github.com>
2026-07-17 11:24:43 +0530

    Accept main versions for remaining conflict files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    environment: 'node'
-  }
-});
+import { defineConfig } from 'vitest/config'; export default defineConfig({test:{environment:'node'}});
