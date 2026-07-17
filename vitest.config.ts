2eb9d7547e5b8773ed77b4171e88b9a6b2160e0a
Prashant Verma <36754148+prashantpv08@users.noreply.github.com>
2026-07-17 11:19:51 +0530

    Resolve PR 7 conflict files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,1 +1,7 @@
-import { defineConfig } from 'vitest/config'; export default defineConfig({test:{environment:'node'}});
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'node'
+  }
+});
